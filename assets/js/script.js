63e95a8e4f1535d05b972bf395f2dc90c483dd59
carolinengari <caroline.ngari@student.moringaschool.com>
2023-08-20 16:41:14 +0300

    conversion to const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-var akan = function(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const akan = function (y, m, d, g) {
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    const maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const d = new Date(y, --m, d);
+    if (g === "Female") {
+        return d && femaleNames[d.getDay()];
+    } else {
+        return d && maleNames[d.getDay()];
+    }
 
-}
+};
