b68a43efe6786dfd77ba045f590f47575687df82
carolinengari <caroline.ngari@student.moringaschool.com>
2023-08-20 17:02:42 +0300

    bug fixes on image links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const akan = function (y, m, d, g) {
     const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     const maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    const d = new Date(y, --m, d);
+    var d = new Date(y, --m, d);
     if (g === "Female") {
         return d && femaleNames[d.getDay()];
     } else {
