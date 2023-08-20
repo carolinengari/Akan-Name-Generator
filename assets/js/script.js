23f1156d2ff1b81f937a271dd31a2b8e4b7939be
carolinengari <caroline.ngari@student.moringaschool.com>
2023-08-20 16:39:57 +0300

    creates and initializes the array values

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var akan = function(y, m, d, g) {
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+}
